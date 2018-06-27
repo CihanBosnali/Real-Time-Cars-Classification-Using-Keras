ca7e83180109b42c8fd87d8966637740508fd163
Cihan Bosnalı <cihanbosna@hotmail.com>
2018-06-27 09:17:50 +0300

    train function added to model_trainer.py

--- model_trainer.py
+++ model_trainer.py
@@ -18,6 +18,7 @@ from keras.layers import Conv2D, MaxPooling2D, Cropping2D
 from keras.callbacks import ModelCheckpoint
 from keras.optimizers import SGD
 
+import random
 # (264, 198, 3)
 
 def create_model(input_shape, num_of_classes=196):
@@ -47,4 +48,67 @@ def create_model(input_shape, num_of_classes=196):
 
     model.add(Dense(num_of_classes, activation="softmax"))    
 
+    model.compile(loss=keras.losses.categorical_crossentropy, optimizer='adam')
+
+    model.summary()
+
     return model
+
+
+def get_matrix(fname):
+    img = cv2.imread(fname)
+    return img 
+
+# Generate data for training
+def generate_data(data_names, labels ,bsize, dlen, reindex):
+    splitpoint = int(0.8*dlen)
+    i = 0
+    while True:
+        x = []
+        y = []
+        for j in range(i,i+bsize):
+            ix = reindex[j]
+            img = get_matrix(images[ix])
+            lbl = np.array([labels[ix]])
+            x.append(img)
+            y.append(lbl)
+        x = np.array(x)
+        y = np.array(y)
+        yield (x,y)
+        i +=bsize
+        if i+bsize > splitpoint:
+            i = 0
+
+# Generate data for validation
+def generate_data_val(data_names, labels ,bsize, dlen, reindex):
+    splitpoint = int(0.8*dlen)
+    i = splitpoint
+    while True:
+        x = []
+        y = []
+        for j in range(i,i+bsize):
+            ix = reindex[j]
+            x.append(get_matrix(images[ix]))
+            y.append(np.array([labels[ix]]))
+        x = np.array(x)
+        y = np.array(y)
+        yield (x,y)
+        i +=bsize
+        if i+bsize > dlen:
+            i = splitpoint
+
+
+def train_model(data_names, labels, model, bsize=16, epochs=10):
+    
+    dlen = len(labels)
+    splitpoint = int(0.8*dlen)
+    reindex = list(range(len(labels)))
+    random.seed(1234)
+    random.shuffle(reindex)
+
+    
+    hs = model.fit_generator(generate_data(data_names, labels ,bsize, dlen, reindex),
+                             steps_per_epoch=int(splitpoint/ bsize),
+                             validation_data=generate_data_val(data_names, labels ,bsize, dlen, reindex),
+                             validation_steps=(dlen-splitpoint)/bsize, epochs=epochs,callbacks=[model_checkpoint])
+    
