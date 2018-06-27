e7004a39ccaeebc4cfbf03072d8159bd7078737f
Cihan Bosnalı <cihanbosna@hotmail.com>
2018-06-27 14:17:59 +0300

    Model changed because of overfitting

--- model_trainer.py
+++ model_trainer.py
@@ -11,7 +11,7 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Lambda
 from keras.layers import Conv2D, MaxPooling2D, Cropping2D
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, TensorBoard
 from keras.optimizers import SGD
 
 import random
@@ -36,24 +36,26 @@ def create_model(input_shape, num_of_classes=196):
     model.add(Lambda(lambda x: (2*x / 255.0) - 1.0, input_shape=input_shape))
 
     model.add(Conv2D(16, (5,5), activation="relu", strides=(2,2)))
-    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding="same"))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(1,1), padding="same"))
 
     model.add(Conv2D(32, (5,5), activation="relu", strides=(2,2)))
-    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding="same"))
-
+    
     model.add(Conv2D(64, (3,3), activation="relu", strides=(2,2)))
-    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding="same"))
+    
+
+    model.add(Conv2D(128, (2,2), activation="relu", strides=(2,2)))
 
     # Classification
     model.add(Flatten())
 
     model.add(Dense(512, activation='relu'))
-
-    model.add(Dropout(0.3))
-
-    model.add(Dense(216, activation='relu'))
+    model.add(Dropout(0.4))
 
     model.add(Dense(216, activation='relu'))
+    model.add(Dropout(0.4))
+
+    model.add(Dense(216, activation='relu'))
+    model.add(Dropout(0.4))
 
     model.add(Dense(num_of_classes, activation="softmax"))    
 
@@ -117,6 +119,25 @@ def generate_data_val(data_names, labels ,bsize, dlen, reindex):
         if i+bsize > dlen:
             i = splitpoint
 
+# Generate data for validation
+def generate_data_before(data_names, labels ,bsize, dlen, reindex):
+    splitpoint = int(0.9*dlen)
+    
+    for i in range(splitpoint, dlen):
+        x = []
+        y = []
+        ix = reindex[i] - 1
+        # -1 because dataset starts from 1 but arrays from 0
+        img = get_matrix(data_names[ix])
+        l = int(labels[ix]) - 1
+        lbl = np.zeros((196))
+        lbl[l] = 1
+        x.append(img)
+        y.append(lbl)
+    x = np.array(x)
+    y = np.array(y)        
+    return (x,y)
+
 def plot_training(hs):
     # Train and validation loss chart
     print(hs.history.keys())
@@ -137,12 +158,23 @@ def train_model(data_names, labels, model, bsize=16, epochs=10):
     random.seed(1234)
     random.shuffle(reindex)
 
+    Board_Callback = TensorBoard(log_dir='./logs',
+                                 histogram_freq=1,
+                                 batch_size=16,
+                                 write_graph=True,
+                                 write_grads=True,
+                                 write_images=True,
+                                 embeddings_freq=0,
+                                 embeddings_layer_names=None)
+
     model_checkpoint = ModelCheckpoint('weights.h5', monitor='val_loss', save_best_only=True)
 
+    valdata = generate_data_before(data_names, labels ,bsize, dlen, reindex)
+    print(len(valdata))
     hs = model.fit_generator(generate_data(data_names, labels ,bsize, dlen, reindex),
                              steps_per_epoch=int(splitpoint/ bsize),
-                             validation_data=generate_data_val(data_names, labels ,bsize, dlen, reindex),
-                             validation_steps=(dlen-splitpoint)/bsize, epochs=epochs,callbacks=[model_checkpoint])
+                             validation_data=valdata,
+                             validation_steps=(dlen-splitpoint)/bsize, epochs=epochs,callbacks=[model_checkpoint, Board_Callback])
 
     plot_training(hs)
 
